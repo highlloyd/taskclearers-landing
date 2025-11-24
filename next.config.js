347871200cccaa416d8d2a42cc0840fa823b2bb1
Ronaldo.Lauture <ronaldo.lauture@gearboxsoftware.com>
2025-11-24 13:19:11 -0600

    fix github pages link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/taskclearers-landing',
+    trailingSlash: true,
     images: {
         unoptimized: true,
         remotePatterns: [
